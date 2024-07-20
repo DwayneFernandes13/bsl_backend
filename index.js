d44d6b8c70fb7dfb9b8c128dda6dedeb8de0be72
DwayneFernandes13 <ferns131097@hotmail.com>
2024-07-20 20:29:58 +0530

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express') //importing express framework for Node JS
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
